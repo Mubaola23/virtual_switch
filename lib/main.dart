3c8264c0c56a6ddab182f4723ae87c40f99b8447
MubaH_dev <mubarakshuaib3@gmail.com>
2025-04-12 22:20:26 +0100

    registered provider class

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:virtual_switch/src/features/home/controller/home_view_controller.dart';
+import 'package:virtual_switch/src/features/home/view/home_view.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => HomeViewController()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'API Data Explorer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+
+      themeMode: ThemeMode.system,
+      home: const DataExplorerScreen(),
+    );
+  }
+}
